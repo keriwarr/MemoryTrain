403c931b11707d5b2d13db6a0a44f610a4a69453
Keri Warr <keri@warr.ca>
2014-09-20 18:14:41 -0400

    adjusting display positioning

--- src/main.c
+++ src/main.c
@@ -8,14 +8,25 @@ static TextLayer *question_layer;
 
 static Layer *window_layer;
 
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
 static void window_load(Window *window) {  
     
     window_layer = window_get_root_layer(window);
-    GRect bounds = layer_get_frame(window_layer);
-    question_layer = text_layer_create(bounds);
+    GRect bounds = layer_get_frame(window_layer);    
+    GRect inner = GRect(0,0,112,168);
+    
+    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_background);
+    s_background_layer = bitmap_layer_create(bounds);
+    bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+    layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+    
+    question_layer = text_layer_create(inner);
     text_layer_set_background_color(question_layer, GColorClear);
     text_layer_set_text_color(question_layer, GColorBlack);
     text_layer_set_text(question_layer, QA[0]);
+    layer_set_bounds((Layer*)question_layer,GRect(0,(168-text_layer_get_content_size(question_layer).h)/2,112,text_layer_get_content_size(question_layer).h));
 
     text_layer_set_font(question_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
     text_layer_set_text_alignment(question_layer, GTextAlignmentCenter);
@@ -26,10 +37,16 @@ static void window_load(Window *window) {
 static void window_unload(Window *window) {
     text_layer_destroy(question_layer);    
     layer_destroy(window_layer);
+    // Destroy GBitmap
+    gbitmap_destroy(s_background_bitmap);
+
+    // Destroy BitmapLayer
+    bitmap_layer_destroy(s_background_layer);
 }
 
 static void init(void) {
   window = window_create();
+    window_set_fullscreen(window, true);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload
