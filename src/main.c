592a90a5e00974ddb86dddf55f81bd59b8e5af09
Keri Warr <keri@warr.ca>
2014-09-20 15:40:43 -0400

    display question

---
+++ src/main.c
@@ -0,0 +1,50 @@
+#include "pebble.h"
+#include "qa.c"
+ 
+static Window *window;
+
+static void window_load(Window *window) {  
+    
+    static TextLayer *question_layer;
+    GRect bounds = layer_get_frame(window_layer);
+    question_layer = text_layer_create(bounds);
+    text_layer_set_background_color(question_layer, GColorClear);
+    text_layer_set_text_color(question_layer, GColorBlack);
+    text_layer_set_text(question_layer, QA[0]);
+
+<<<<<<< HEAD
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  layer_get_frame
+  GRect frame = window_frame = layer_get_frame(window_layer);
+=======
+    text_layer_set_font(question_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    text_layer_set_text_alignment(question_layer, GTextAlignmentCenter);
+
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(question_layer));
+>>>>>>> 0ec869710807ef8d6544403c7a9efceffd48db0b
+}
+
+static void window_unload(Window *window) {
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload
+  });
+  window_stack_push(window, true /* Animated */);
+  window_set_background_color(window, GColorBlack);
+}
+
+static void deinit(void) {
+  window_destroy(window);
+    text_layer_destroy(question_layer);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
